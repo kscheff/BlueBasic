7d8b917b61cd4ed587ee67d5d416b2dc56f12b94
Kai Scheffer <kai@sffr.de>
2017-12-30 21:39:34 +0100

    fix too long device info timestamp in batch file in order not to mess with different version file name

--- BLE-CC254x-1.4.2.2/Projects/ble/common/cc2540/timestamp.bat
+++ BLE-CC254x-1.4.2.2/Projects/ble/common/cc2540/timestamp.bat
@@ -15,5 +15,7 @@ goto :eof
 set hour=%time:~0,2%
 if %hour% lss 10 (set hour=0%time:~1,1%)
 set timestamp=%yy%%mm%%dd%%hour%%time:~3,2%%time:~6,2%
-echo #define BUILD_TIMESTAMP "%2/%timestamp%" > "%3/timestamp.h"
+set prefix=%2
+set prefix=%prefix:~0,8%
+echo #define BUILD_TIMESTAMP "%prefix%/%timestamp%" > "%3/timestamp.h"
 echo %timestamp% > "%1\..\..\..\..\..\hex\BlueBasic-%2.version"
