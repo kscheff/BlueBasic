8a51f11b4f8c89d16ca6cc4f10690bc541eca982
Kai Scheffer <kai@diescheffers.de>
2018-11-24 23:57:30 +0100

    adding simply copy script

---
+++ copy_hex_to_bluebattery.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp hex/BlueBasic-BlueBattery.bin ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0000
+cp hex/BlueBasic-BlueBattery.hex ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0000
+cp hex/BlueBasic-BlueBattery.version ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0000
+cp hex/BlueBasic-BlueBattery.bin ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0003
+cp hex/BlueBasic-BlueBattery.version ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0003
+cp hex/BlueBasic-BlueBattery-D1.bin ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0004/BlueBasic-BlueBattery.bin
+cp hex/BlueBasic-BlueBattery-D1.version ~/repositories/BlueBattery-iOS/Source/BlueSolar/PID_0004/BlueBasic-BlueBattery.version
