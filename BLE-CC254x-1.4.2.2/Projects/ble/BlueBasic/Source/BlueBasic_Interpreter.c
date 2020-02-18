kscheff/BlueBasic

commit 95101356e315ed1fc7d825d967437cd9e41118b3

Fix BlueSolar not writing history: OPEN APPEND w/o modulo, record did not append data record as expected

BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Source/BlueBasic_Interpreter.c
@@ -3616,9 +3616,10 @@ cmd_open:
         if (bSnv)
           GOTO_QWHAT;
         file->action = 'W';
-        for (unsigned long special = FS_MAKE_FILE_SPECIAL(file->filename, 0); flashstore_findspecial(special); special++)
+        unsigned short limit = file->record;
+        for (unsigned long special = FS_MAKE_FILE_SPECIAL(file->filename, 0); flashstore_findspecial(special); special++, file->record++)
         {
-          if (hasOffset && (unsigned short) (special & 0xffff) >= file->record)
+          if (hasOffset && (unsigned short) (special & 0xffff) >= limit)
           {
             break;
           }

BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Build/BlueBattery-D1/Obj/timestamp.h
@@ -1,1 +1,1 @@
-#define BUILD_TIMESTAMP "BlueBatt/20190928010350" 
+#define BUILD_TIMESTAMP "BlueBatt/20191213104622" 

BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Build/BlueBattery-MPPT/Obj/timestamp.h
@@ -1,1 +1,1 @@
-#define BUILD_TIMESTAMP "BlueBatt/20191006190549" 
+#define BUILD_TIMESTAMP "BlueBatt/20191213104638" 

commit 6de5a0dfc61f28bfc9d5e971824e44fea2dcbae5

Fix  OPEN APPEND with given modulo and record (was broken by 95101356e315ed1fc7d825d967437cd9e41118b3)

BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Source/BlueBasic_Interpreter.c
@@ -3616,10 +3616,11 @@ cmd_open:
         if (bSnv)
           GOTO_QWHAT;
         file->action = 'W';
-        unsigned short limit = file->record;
+        unsigned short record = file->record;
+        file->record = 0;
         for (unsigned long special = FS_MAKE_FILE_SPECIAL(file->filename, 0); flashstore_findspecial(special); special++, file->record++)
         {
-          if (hasOffset && (unsigned short) (special & 0xffff) >= limit)
+          if (hasOffset && (unsigned short) (special & 0xffff) >= record)
           {
             break;
           }