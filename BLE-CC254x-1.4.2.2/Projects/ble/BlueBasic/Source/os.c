afb4a317c64af71c1adae6f77b393e0d23c5405e
Kai Scheffer <kai@sffr.de>
2020-09-25 16:45:31 +0200

    not used in UART driver

--- BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Source/os.c
+++ BLE-CC254x-1.4.2.2/Projects/ble/BlueBasic/Source/os.c
@@ -491,8 +491,8 @@ unsigned char OS_serial_open(unsigned char port, unsigned long baud, unsigned ch
   config.flowControl = flow == 'H' ? HAL_UART_FLOW_ON : HAL_UART_FLOW_OFF;
   config.flowControlThreshold = flow == 'H' ? 64 : 0;
   config.idleTimeout = 0;
-  config.rx.maxBufSize = HAL_UART_DMA_RX_MAX;
-  config.tx.maxBufSize = HAL_UART_DMA_TX_MAX;
+  config.rx.maxBufSize = 0; //HAL_UART_DMA_RX_MAX;
+  config.tx.maxBufSize = 0; //HAL_UART_DMA_TX_MAX;
   config.intEnable = 1;
 #if UART_USE_CALLBACK  
   config.callBackFunc = _uartCallback;
