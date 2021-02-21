6aa8af1750cf26bb56586fe526423d2dd4b7e8fd
Kai Scheffer <kai@sffr.de>
2021-02-21 20:41:01 +0100

    Update BLE Stack to 1.5.1.1

--- BLE-CC254x-1.5.0.16/Projects/ble/Profiles/DevInfo/devinfoservice.c
+++ BLE-CC254x-1.5.0.16/Projects/ble/Profiles/DevInfo/devinfoservice.c
@@ -166,7 +166,7 @@ static uint8 devInfoSystemId[DEVINFO_SYSTEM_ID_LEN] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 // Model Number String characteristic
 static CONST uint8 devInfoModelNumberProps = GATT_PROP_READ;
-static CONST uint8 devInfoModelNumber[] = "BLE-1.5.0.16";
+static CONST uint8 devInfoModelNumber[] = "BLE-1.5.1.1";
 
 #ifndef DEVINFO_SHORTER_HEADER
 // Serial Number String characteristic
