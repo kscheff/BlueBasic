b8321e22c77b1fa4a3587cae6dd9d83884d76bc1
Kai Scheffer <kai@diescheffers.de>
2021-11-19 11:33:57 +0100

    blink LED on both ports P1.0 and P0.4

--- BLE-CC254x-1.5.0.16/Projects/ble/BlueBasicLoader/Source/BlueBasicLoader.h
+++ BLE-CC254x-1.5.0.16/Projects/ble/BlueBasicLoader/Source/BlueBasicLoader.h
@@ -27,11 +27,14 @@ extern "C"
 #define LED_BLE_INIT \
   P1SEL = 0;\
   P1DIR = 0x01;\
-  P1 = 0;
-#define LED_BLE_ON P1 |= 1
-#define LED_BLE_OFF P1 &= ~1
-#define LED_BLE_TOGGLE P1 ^= 1
-#define LED_BLE(a) P1 = ((P1 & ~1) | (a))
+  P1 = 0;\
+  P0SEL = 0;\
+  P0DIR = 0x10;\
+  P0 = 0;
+#define LED_BLE_ON P1 |= 1, P0 |= 0x10
+#define LED_BLE_OFF P1 &= ~1, P0 &= ~0x10
+#define LED_BLE_TOGGLE P1 ^= 1, P0 ^= 0x10
+#define LED_BLE(a) P1 = ((P1 & ~1) | (a)), P0 = ((P0 & ~0x10) | ((a>0)<<4))
 #endif  
   
 /*********************************************************************
